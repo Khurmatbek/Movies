e98649d91643255c5b671ba33e5cd7e5d8577450
 MacBook <macbook@MacBook-Pro-MacBook.local>
2023-11-10 10:35:10 +0500

    new tag add

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+const elFirstList = document.querySelector(".first-section__list");
+const elFisrtItem = document.querySelector(".first-section__item");
+console.log(elFirstList, elFisrtItem);
+const FirstTemplate = document.querySelector(".first-section__template").content;
+const fragment = new DocumentFragment();
+
+
+
